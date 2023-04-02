8a79ff6567f4e2b0a8cab05e1ed2767a86d6d2c4
MarinaKolbina <marianna.hana@gmail.com>
2023-04-02 23:42:04 +0200

    tests

--- Tracker/NewBehaviorViewController.swift
+++ Tracker/NewBehaviorViewController.swift
@@ -8,7 +8,17 @@
 import Foundation
 import UIKit
 
-class NewBehaviorViewController: UIViewController {
+class NewBehaviorViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.rowHeight = 75
+        table.backgroundColor = UIColor(named: "grey_for_textField")
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.layer.cornerRadius = 16
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        return table
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,52 +31,52 @@ class NewBehaviorViewController: UIViewController {
         textField.backgroundColor = UIColor(named: "grey_for_textField")
         view.addSubview(textField)
         
-        let tableView = UITableView()
-        tableView.rowHeight = 75
-        tableView.backgroundColor = UIColor(named: "grey_for_textField")
+
         // Регистрируем класс UITableViewCell для ячейки таблицы
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
-        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
         
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.layer.cornerRadius = 16
         NSLayoutConstraint.activate([
-            textField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 73),
+            textField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 38),
             textField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             textField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
             textField.heightAnchor.constraint(equalToConstant: 75)
         ])
         
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.layer.cornerRadius = 16
+        view.addSubview(tableView)
+        
         NSLayoutConstraint.activate([
-            tableView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
-            tableView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)
-//            tableView.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 24)
-        ])
+                    tableView.topAnchor.constraint(equalTo:textField.bottomAnchor, constant: 24),
+                    tableView.leadingAnchor.constraint(equalTo:view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+                    tableView.trailingAnchor.constraint(equalTo:view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+                    tableView.bottomAnchor.constraint(equalTo:view.safeAreaLayoutGuide.bottomAnchor),
+                ])
 
+        
     }
-        func numberOfSections(in tableView: UITableView) -> Int {
-            return 1
-        }
-        
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return 2
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            // Получаем ячейку таблицы
-            let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
-            
-            // Настраиваем текст ячейки
-            if indexPath.row == 0 {
-                cell.textLabel?.text = "Категория"
-            } else {
-                cell.textLabel?.text = "Расписание"
-            }
-            
-            return cell
-        }
-
-}
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Получаем ячейку таблицы
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        
+        // Настраиваем текст ячейки
+        if indexPath.row == 0 {
+            cell.textLabel?.text = "Категория"
+        } else {
+            cell.textLabel?.text = "Расписание"
+        }
+        
+        return cell
+    }
+    
+}
+
